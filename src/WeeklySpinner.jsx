ce2e7dacf9ddd4b31eb11aacdb517596491f9af1
Vaibhav Kore <101107216+Vaibhav240804@users.noreply.github.com>
2023-08-03 11:33:23 +0530

    Create WeeklySpinner.jsx

---
+++ src/WeeklySpinner.jsx
@@ -0,0 +1,12 @@
+import  spinGif from "./static/spinnerGIF.gif"
+
+function WeeklySpin() {
+  return (
+        <>
+        <h3 className="">loading</h3>
+      <img className="max-h-fit max-w-fit" src={spinGif} alt="" />
+      </>
+  )
+}
+
+export default WeeklySpin
